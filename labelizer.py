e3a3f46db99a7ee9de1ddc09d9b6e3ed541f67e6
mrdavidoneill <mrdavidoneillll@gmail.com>
2020-07-28 12:32:41 +0200

    Changed app.run to include debug options, for easier development

--- labelizer.py
+++ labelizer.py
@@ -49,4 +49,4 @@ def upload_file():
     return render_template("home.html")
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, use_debugger=False, use_reloader=True, passthrough_errors=True)
