a2ab2c85d65a0d84b7498d68b00631603203bfce
mrdavidoneill <mrdavidoneillll@gmail.com>
2020-08-02 18:01:02 +0200

    remove print statement

--- labelizer.py
+++ labelizer.py
@@ -6,10 +6,6 @@ environmentvariables.load()
 from app import app, db
 from app.models import User
 
-
-print(os.environ.get('SECRET_KEY'))
-
-
 @app.shell_context_processor
 def make_shell_context():
     """ Adds db and User models when using 'flask shell' commandflask  """
