4766991cb0ed451713909c427dc954105837ac7d
mrdavidoneill <mrdavidoneillll@gmail.com>
2020-08-02 18:09:29 +0200

    Added secretkey backup

--- app/config.py
+++ app/config.py
@@ -4,7 +4,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY') or \
+        "something-for-debugging-only-6544#]`()!)54543qqwdqw1112vm"
     UPLOAD_FOLDER = 'upload'
     OUTPUT_FOLDER = 'output'
     ALLOWED_EXTENSIONS = {'pdf'}
